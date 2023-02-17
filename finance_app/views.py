069c3801437da87dfb26d21095678d3e9e77b5b1
Samuel Massnick <smassnick24@adrian.edu>
2023-02-17 13:10:03 -0500

    added date from SQLite3 database directly into templates using views instead of dummy data

--- finance_app/views.py
+++ finance_app/views.py
@@ -2,41 +2,43 @@ from django.shortcuts import render
 from django.utils import timezone
 import datetime
 from finance_app.models import Purchase, Income
+
+
 # creating dummy data to push to templates for testing
 
-purchase_list = [
-    {
-        'purchase_name': "Gasoline",
-        'purchase_description': "Gas for school",
-        'amount': 40.00,
-        'date_purchased': str(datetime.datetime.now()),
-        'owner': "smassnick24"
-    },
-    {
-        'purchase_name': "Snacks",
-        'purchase_description': "Snacks for aubrey",
-        'amount': 20.17,
-        'date_purchased': str(datetime.datetime.now()),
-        'owner': "smassnick24"
-    },
-]
+# purchase_list = [
+#     {
+#         'purchase_name': "Gasoline",
+#         'purchase_description': "Gas for school",
+#         'amount': 40.00,
+#         'date_purchased': str(datetime.datetime.now()),
+#         'owner': "smassnick24"
+#     },
+#     {
+#         'purchase_name': "Snacks",
+#         'purchase_description': "Snacks for aubrey",
+#         'amount': 20.17,
+#         'date_purchased': str(datetime.datetime.now()),
+#         'owner': "smassnick24"
+#     },
+# ]
 
-income_list = [
-    {
-        "income_source": "Wesco",
-        "income_description": "paycheck from wesco",
-        "amount": 200.13,
-        "date_received": str(datetime.datetime.now()),
-        "owner": "smassnick24"
-    },
-    {
-        "income_source": "Wesco",
-        "income_description": "paycheck from wesco",
-        "amount": 313.13,
-        "date_received": str(datetime.datetime.now()),
-        "owner": "smassnick24"
-    }
-]
+# income_list = [
+#     {
+#         "income_source": "Wesco",
+#         "income_description": "paycheck from wesco",
+#         "amount": 200.13,
+#         "date_received": str(datetime.datetime.now()),
+#         "owner": "smassnick24"
+#     },
+#     {
+#         "income_source": "Wesco",
+#         "income_description": "paycheck from wesco",
+#         "amount": 313.13,
+#         "date_received": str(datetime.datetime.now()),
+#         "owner": "smassnick24"
+#     }
+# ]
 
 def purchase_page(request):
     context = {
