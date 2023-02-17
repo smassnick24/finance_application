d8b2222d138a9769128924822d7294eb48b60682
Samuel Massnick <smassnick24@adrian.edu>
2023-02-17 11:59:11 -0500

    Modified vies and added Html files

--- finance_app/views.py
+++ finance_app/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 def finance_app(request):
     return render(request, "finance_app/home.html")
 
