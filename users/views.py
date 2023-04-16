b6e5e454a746237f3a0b9688e5d47ce6239ecf2d
Samuel Massnick <smassnick24@adrian.edu>
2023-04-16 12:10:37 -0400

    fixed errors and added profile functionalitites

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == "POST":
@@ -14,8 +15,13 @@ def register(request):
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
 
+@login_required
 def profile(request):
+    user_form = UserUpdateForm(instance=request.user)
+    profile_form = ProfileUpdateForm(instance=request.user.profile)
     context = {
-        "user": request.user
+        "user": request.user,
+        "update_user": user_form,
+        "update_profile": profile_form,
     }
     return render(request, "users/profile.html", context)
