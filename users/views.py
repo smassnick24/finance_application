23d0030471866a4a9e5e75978b632e19a8cb8e10
Samuel Massnick <smassnick24@adrian.edu>
2023-04-16 13:46:10 -0400

    improved profile

--- users/views.py
+++ users/views.py
@@ -17,8 +17,20 @@ def register(request):
 
 @login_required
 def profile(request):
-    user_form = UserUpdateForm(instance=request.user)
-    profile_form = ProfileUpdateForm(instance=request.user.profile)
+    if request.method == "POST":
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+    
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f"{request.user}'s Profile Updated Successfully")
+            return redirect('profile')
+        
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+   
     context = {
         "user": request.user,
         "update_user": user_form,
